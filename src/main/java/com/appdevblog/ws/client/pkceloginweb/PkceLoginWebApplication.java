e7fb1e96fe3fdb075eed00a2ce513abfdc30c49e
mahilw <mahilw@cclk.lk>
2022-05-25 21:46:05 +0530

    oauth testing pkce javascript

---
+++ src/main/java/com/appdevblog/ws/client/pkceloginweb/PkceLoginWebApplication.java
@@ -0,0 +1,13 @@
+package com.appdevblog.ws.client.pkceloginweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkceLoginWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkceLoginWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/appdevblog/ws/client/pkceloginweb/PkceLoginWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appdevblog.ws.client.pkceloginweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PkceLoginWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
